3fc5b15e6f7306ddc74b2228355a6d7d21f92395
Alarlar <52812499+Alarlar@users.noreply.github.com>
2025-04-05 17:08:43 -0700

    mapping of plan  array to list items with key and title

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
+
+const trainingPlan = [
+  { id: 1, title: "Sleep"},
+  { id: 2, title: "Train"},
+  { id: 3, title: "Make $"}
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+ return (
+    <div>
+      <h1>Training Plan</h1>
+      <ul> 
+        {trainingPlan.map((plan) => 
+          <li key={plan.id}>{plan.title}</li>
+      )}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
