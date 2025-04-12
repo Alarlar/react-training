bd133afacd470e8fd6e70b528911e201ea9112ca
Alarlar <52812499+Alarlar@users.noreply.github.com>
2025-04-11 22:07:06 -0700

    Added AddTodoForm companent to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import './App.css';
-
-const trainingPlan = [
-  { id: 1, title: "Sleep"},
-  { id: 2, title: "Train"},
-  { id: 3, title: "Make $"}
-];
+import ToplanList from './toplanList';
+import AddTodoForm from './AddTodoForm';
 
 function App() {
  return (
     <div>
       <h1>Training Plan</h1>
-      <ul> 
-        {trainingPlan.map((plan) => 
-          <li key={plan.id}>{plan.title}</li>
-      )}
-      </ul>
+      <AddTodoForm />
+      <ToplanList />
     </div>
   );
 }
